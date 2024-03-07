8595610f7e201f24aa25b17ada08bb6d7ca8efce
Tan Jia He <jiahe.tan@bertelsmann.de>
2024-03-07 15:06:41 +0800

    [W2-Assessment]-initialized

---
+++ src/main/java/com/example/W2Assessment/W2AssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.W2Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2AssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/W2Assessment/W2AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.W2Assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
